17b18eb76af53803c19733cb5aebaaf9150d8ba1
dind300 <60182756+dind300@users.noreply.github.com>
2020-02-09 10:12:26 -0800

    Update isLinkedListCycle.java

--- isLinkedListCycle.java
+++ isLinkedListCycle.java
@@ -1,27 +1,36 @@
 
 public class isLinkedListCycle {
+
 	public static void main(String[] args) {
 		Node node1 = new Node(12);
 		Node node2 = new Node(3, node1);
 		Node node3 = new Node(7, node2);
 		Node node4 = new Node(6, node3);
-		//node1.setNext(node3);
-		System.out.println(isLinkedListCycle(node1));
+		node1.setNext(node3);
+		System.out.println(isLinkedListCycle(node4));
 	}
 
 	public static boolean isLinkedListCycle(Node node) {
 		Node pointer1=node;
 		Node pointer2=node;
-		while(pointer2.getNext()!=null) {
+		boolean flag=false;
+		while(!flag) {
+			pointer2=pointer2.getNext();
+			if(pointer1==pointer2) {
+				return true;
+			}
 			pointer1=pointer1.getNext();
-			pointer2=pointer2.getNext().getNext();
 			if(pointer1==pointer2) {
 				return true;
 			}
 			if(pointer2==null||pointer1==null) {
 				return false;
 			}
+			pointer2=pointer2.getNext();
+			if(pointer2==null) {
+				return false;
 			}
+		}
 		return false;
 	}
 }
